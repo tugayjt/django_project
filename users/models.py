8fc1c8698ea67d3033c22c57d0471aa24a8ef167
tugayjt <tugayjabbarov@gmail.com>
2024-12-07 16:28:28 +0400

    configurations

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-
+from PIL import Image,UnidentifiedImageError
+import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,12 +10,18 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Save the user first
 
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        # Only process the image if it exists
+        if self.image and os.path.exists(self.image.path):
+            try:
+                img = Image.open(self.image.path)
+                # Resize the image if it exceeds 300x300
+                if img.height > 300 or img.width > 300:
+                    output_size = (300, 300)
+                    img.thumbnail(output_size)
+                    img.save(self.image.path)
+            except (OSError, UnidentifiedImageError) as e:
+                print(f"Error processing image {self.image.path}: {e}")
+                # Optionally, handle this by logging or setting a default image
